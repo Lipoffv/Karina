a3847cb76c328936b3c3f3ba48aed25f42adc128
Karina <SidKarina2001@mail.ru>
2020-07-18 23:40:35 +0300

    initiul

--- zad_9.py
+++ zad_9.py
@@ -3,7 +3,7 @@ import math
 a = float(input("a = "))
 b = float(input("b = "))
 c = float(input("c = "))
-D = b ** 2 - 4 * a * c
+D = b ** 2 + ((-1)*4 * a * c)
 print(D)
 if D == 0:
     x1 = (-b) / (2 * a)
@@ -17,3 +17,5 @@ else:
 
 
 # TODO Доделать решение для двух корней. Работает некорректно.
+
+# сделяль
